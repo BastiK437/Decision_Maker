05493164c389444c75bbf0f095f25977ab56ace1
Bastian Krammer <bastiankrammer@googlemail.com>
2019-10-04 13:54:57 +0200

    switched to javascript

---
+++ decisionBot.js
@@ -0,0 +1,40 @@
+// require the discord.js module
+const Discord = require('discord.js');
+
+// create a new Discord client
+const client = new Discord.Client();
+
+function getRandomInt(max) {
+	return Math.floor(Math.random() * Math.floor(max));
+}
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// login to Discord with your app's token
+const fs = require('fs');
+const textByLine = fs.readFileSync('token').toString();
+
+client.login(textByLine);
+
+client.on('message', message => {
+	// console.log(message.content);
+
+	// const str = message.content;
+	// const pos = str.search('!DM');
+	const name = message.author;
+
+	if (message.content === '!DM') {
+		const random = getRandomInt(2);
+		if (random === 0) {
+			message.channel.send ('Geh ins Bett ' + name + '!');
+		} else if (random === 1) {
+			message.channel.send('Spiel noch eine Runde ' + name + '!');
+		} else {
+			message.channel.send('error');
+		}
+	}
+});

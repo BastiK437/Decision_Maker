efc491666a48b2edaa4a8d3219fc226c50bb920a
Bastian Krammer <Bastian.Krammer@haw-hamburg.de>
2019-11-13 00:05:37 +0100

    debug off

--- decisionBot.js
+++ decisionBot.js
@@ -5,7 +5,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 // DEBUG modus
-const DEBUG = true;
+const DEBUG = false;
 
 function getRandomInt(max) {
 	return Math.floor(Math.random() * Math.floor(max));

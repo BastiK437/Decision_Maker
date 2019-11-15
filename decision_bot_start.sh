7cb8d82b1ef1f74038d66440d24bd16a3006218f
Bastian Krammer <Bastian.Krammer@haw-hamburg.de>
2019-11-15 22:04:50 +0100

    wip

--- decision_bot_start.sh
+++ decision_bot_start.sh
@@ -2,11 +2,31 @@
 
 pid=$(ps -e | grep node | awk '{print $1}')
 
+echo $pid
+
 if [ pid>0 ] 
   then 
     kill pid # kill the running instance of the bot
 fi
 
+if [ "$1"=="start"] # start if
+  then
+
+    # read the token from file
+    input="/home/pi/Workspace/Javascript/Decision_Maker/token"
+    while IFS= read -r line
+    do
+      token=$line
+    done < "$input"
+
+    # start the bot
+    node /home/pi/Workspace/Javascript/Decision_Maker/decisionBot.js $token
+
+  elif [ "$1"=="kill" ]
+    then
+      # bot already got killed
+
+fi # end if
 
 
 

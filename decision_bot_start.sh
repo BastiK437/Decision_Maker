fc465296dabec52b6d5ec2676ee9e4bbfa11dd79
Bastian Krammer <Bastian.Krammer@haw-hamburg.de>
2019-11-15 21:57:11 +0100

    start script update

--- decision_bot_start.sh
+++ decision_bot_start.sh
@@ -1,17 +1,29 @@
 #!/bin/sh
 #input="~Workspace/Javascript/Decision_Maker/token"
 
-if [ $(ps -e | grep node | awk '{print $1}')>0 ] 
-	then 
-	echo "Bot laeuft"
-	kill $(ps -e | grep node | awk '{print $1}')
+if [ $(ps aux | grep node | awk '{print $2}')>0 ] 
+  then 
+    kill $(ps aux | grep node | awk '{print $2}') # kill the running instance of the bot
 fi
 
-input="/home/pi/Workspace/Javascript/Decision_Maker/token"
-while IFS= read -r line
-do
-  token=$line
-done < "$input"
+if [ "$1"=="start"] # start if
+  then
 
-#echo "$token"
-node /home/pi/Workspace/Javascript/Decision_Maker/decisionBot.js $token &
+    # read the token from file
+    input="/home/pi/Workspace/Javascript/Decision_Maker/token"
+    while IFS= read -r line
+    do
+      token=$line
+    done < "$input"
+
+    # start the bot
+    node /home/pi/Workspace/Javascript/Decision_Maker/decisionBot.js $token
+
+  elif [ "$1"=="kill" ]
+    then
+      # bot already got killed
+
+fi # end if
+
+
+

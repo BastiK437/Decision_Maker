29110bdc7288bfa87435d5e47de28c0a944013fc
Bastian Krammer <Bastian.Krammer@haw-hamburg.de>
2019-11-15 22:04:17 +0100

    wip

--- decision_bot_start.sh
+++ decision_bot_start.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-#input="~Workspace/Javascript/Decision_Maker/token"
 
-if [ $(ps aux | grep node | awk '{print $2}')>0 ] 
+pid=$(ps -e | grep node | awk '{print $1}')
+
+if [ pid>0 ] 
   then 
-    kill $(ps aux | grep node | awk '{print $2}') # kill the running instance of the bot
+    kill pid # kill the running instance of the bot
 fi
 
 

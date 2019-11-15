0ffe5cfebc73bbc2e1390e829bb09d494e2afa10
Bastian Krammer <Bastian.Krammer@haw-hamburg.de>
2019-11-15 22:28:54 +0100

    v1.0.1

--- decision_bot_start.sh
+++ decision_bot_start.sh
@@ -2,7 +2,7 @@
 # Autor: Bastian Krammer
 # Erstellt am: 10.11.2019
 # Github: TODO
-# Version: 1.0.0
+# Version: 1.0.1
 
 
 pid=$(ps -e | grep 'node' | awk '{print $1}')
@@ -10,28 +10,26 @@ pid=$(ps -e | grep 'node' | awk '{print $1}')
 if [ $pid>0 ] 
   then 
     kill $pid # kill the running instance of the bot
-    echo "killed old bot"
     reset
 fi
 
-if [ "$1" == "start" ] # start if
+if [ "$1" == "kill" ] # start if
   then
+    echo "killed old bot"    
 
-    # read the token from file
-    input="/home/pi/Workspace/Javascript/Decision_Maker/token"
-    while IFS= read -r line
-    do
-      token=$line
-    done < "$input"
+  else
+    then
+      # read the token from file
+      input="/home/pi/Workspace/Javascript/Decision_Maker/token"
+      while IFS= read -r line
+      do
+        token=$line
+      done < "$input"
 
-    # start the bot
-    node /home/pi/Workspace/Javascript/Decision_Maker/decisionBot.js $token &
+      # start the bot
+      node /home/pi/Workspace/Javascript/Decision_Maker/decisionBot.js $token &
 
-    echo "start new bot"
-
-  #elif [ "$1"=="kill" ]
-  #  then
-      # bot already got killed
+      echo "start new bot"
 
 fi # end if
 

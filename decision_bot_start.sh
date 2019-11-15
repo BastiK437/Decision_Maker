73e4524ca64d4088dd961c2358279c317c030608
Bastian Krammer <bastiankrammer@googlemail.com>
2019-11-15 21:51:44 +0100

    start

--- decision_bot_start.sh
+++ decision_bot_start.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 #input="~Workspace/Javascript/Decision_Maker/token"
+
+if [ $(ps -e | grep node | awk '{print $1}')>0 ] 
+	then 
+	echo "Bot laeuft"
+	kill $(ps -e | grep node | awk '{print $1}')
+fi
+
 input="/home/pi/Workspace/Javascript/Decision_Maker/token"
 while IFS= read -r line
 do
@@ -7,4 +14,4 @@ do
 done < "$input"
 
 #echo "$token"
-node /home/pi/Workspace/Javascript/Decision_Maker/decisionBot.js $token
+node /home/pi/Workspace/Javascript/Decision_Maker/decisionBot.js $token &

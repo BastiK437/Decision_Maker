0b61461f2a9c727852570f9c03260226c07006a0
Bastian Krammer <Bastian.Krammer@haw-hamburg.de>
2019-11-15 22:54:13 +0100

    fix

--- decisionBot.js
+++ decisionBot.js
@@ -29,103 +29,104 @@ const token = process.argv[2];
 client.login(token);
 
 client.on('message', message => {
-	// console.log(message.content);
-	
-	var sendMsg;
-	const name  = message.author;
-	const date  = new Date();
-	
-	// 0-6 
-	// 0 sunday, 1 monday, 2 tuesday, 3 wednesday, 4 thursday, 5 friday, 6 saturday, 7 sunday
-	const day   = date.getDay();
-	// 0-23
-	const hours = date.getHours();
-	
-	var timeFac = 0.0;
-	var weekFac = 0.0;
-	var randomFac = 0.0;
-
-	if(DEBUG) {
-		console.log("Day: " + day);
-		console.log("Hour: " + hours);
-	}
-
-	if(day <= 4) {
-		// during the week
-		weekFac = 0.7;
-	}else {
-		// weekend
-		weekFac = 0.55;
-	}
-
-	if( hours >=3 && hours <= 10 ) {
-		timeFac = 0.7;
-	}else if( hours >= 11 && hours <= 20 ) {
-		timeFac = 0.0;
-	}else {
-		switch(hours){
-			// 22 till 3 o'clock increase fac
-			case 21:
-				timeFac = 0.1;
-				break;
-			case 22:
-				timeFac = 0.2;
-				break;
-			case 23:
-				timeFac = 0.3;
-				break;
-			case 0:
-				timeFac = 0.4;
-				break;
-			case 1: 
-				timeFac = 0.5;
-				break;
-			case 2: 
-				timeFac = 0.6;
-				break;
-		}
-	}
-
-	randomFac = timeFac + weekFac;
-	
-	var random = getRandomInt(100) * randomFac;
-
-	if(DEBUG) {
-		console.log('Time fac: ' + randomFac);
-		console.log('Final random Number: ' + random);
-	}
-
-	switch(message.content) {
-		case '!DM Bett':
-		case '!DM bett':
-			if (random >= 50) {	// go to bed
-				sendMsg = 'Geh ins Bett ' + name + '!';
-			} else if (random < 50) { // stay online
-				sendMsg = 'Zock noch eine Runde ' + name + '!';
-			}
-			break;
-		case '!DM Bier':
-		case '!DM bier':
-			if (random >= 50) {	// stop drinking
-				sendMsg = 'Trink kein Bier mehr ' + name + '!';
-			} else if (random < 50) { // drink one more
-				sendMsg = 'Trink noch ein Bier ' + name + '!';
-			}
-			break;
-		case '!DM Runde':
-		case '!DM runde':
-			if (random >= 50) {	// stop drinking
-				sendMsg = 'Alt + F4 ' + name + '!';
-			} else if (random < 50) { // drink one more
-				sendMsg = 'Zock noch eine Runde ' + name + '!';
-			}
-			break;
-		default:
-			sendMsg = 'Available commands: bett, bier or runde. NOT case sensitive';
-			break;
-	}
 
 	if( message.content.includes('!DM') ){
+		// console.log(message.content);
+		
+		var sendMsg;
+		const name  = message.author;
+		const date  = new Date();
+		
+		// 0-6 
+		// 0 sunday, 1 monday, 2 tuesday, 3 wednesday, 4 thursday, 5 friday, 6 saturday, 7 sunday
+		const day   = date.getDay();
+		// 0-23
+		const hours = date.getHours();
+		
+		var timeFac = 0.0;
+		var weekFac = 0.0;
+		var randomFac = 0.0;
+
+		if(DEBUG) {
+			console.log("Day: " + day);
+			console.log("Hour: " + hours);
+		}
+
+		if(day <= 4) {
+			// during the week
+			weekFac = 0.7;
+		}else {
+			// weekend
+			weekFac = 0.55;
+		}
+
+		if( hours >=3 && hours <= 10 ) {
+			timeFac = 0.7;
+		}else if( hours >= 11 && hours <= 20 ) {
+			timeFac = 0.0;
+		}else {
+			switch(hours){
+				// 22 till 3 o'clock increase fac
+				case 21:
+					timeFac = 0.1;
+					break;
+				case 22:
+					timeFac = 0.2;
+					break;
+				case 23:
+					timeFac = 0.3;
+					break;
+				case 0:
+					timeFac = 0.4;
+					break;
+				case 1: 
+					timeFac = 0.5;
+					break;
+				case 2: 
+					timeFac = 0.6;
+					break;
+			}
+		}
+
+		randomFac = timeFac + weekFac;
+		
+		var random = getRandomInt(100) * randomFac;
+
+		if(DEBUG) {
+			console.log('Time fac: ' + randomFac);
+			console.log('Final random Number: ' + random);
+		}
+
+		switch(message.content) {
+			case '!DM Bett':
+			case '!DM bett':
+				if (random >= 50) {	// go to bed
+					sendMsg = 'Geh ins Bett ' + name + '!';
+				} else if (random < 50) { // stay online
+					sendMsg = 'Zock noch eine Runde ' + name + '!';
+				}
+				break;
+			case '!DM Bier':
+			case '!DM bier':
+				if (random >= 50) {	// stop drinking
+					sendMsg = 'Trink kein Bier mehr ' + name + '!';
+				} else if (random < 50) { // drink one more
+					sendMsg = 'Trink noch ein Bier ' + name + '!';
+				}
+				break;
+			case '!DM Runde':
+			case '!DM runde':
+				if (random >= 50) {	// stop drinking
+					sendMsg = 'Alt + F4 ' + name + '!';
+				} else if (random < 50) { // drink one more
+					sendMsg = 'Zock noch eine Runde ' + name + '!';
+				}
+				break;
+			default:
+				sendMsg = 'Available commands: bett, bier or runde. NOT case sensitive';
+				break;
+		}
+		
 		message.channel.send(sendMsg);
 	}
 });
